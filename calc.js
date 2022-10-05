c2c327669a770fe8bdec1c607c150023257cbf8a
Mohamd Imad <medoimad@hotmail.com>
2022-10-04 21:45:52 -0400

    Add the BMI calculator

--- calc.js
+++ calc.js
@@ -4,10 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //Require the body-parser package
 
-
 const app = express();
 //app is a function that represents the express module
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -19,18 +17,19 @@ app.use(bodyParser.urlencoded({
 app.listen(3000, function() {
   console.log("server started on port 3000")
 });
+//This utilizes the listen method of the express module which will listen to the port 3000 of your server
 
 app.get("/about", function(req, res) {
   res.send("This is a simple Calculator file");
 });
 // app.get is a method provided by express and it allows our server to react when a browser attempts to access the server by making a get request
+// when we access the /about directory we will get "This is a simple Calculator file" back
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 // app.get is a method provided by express and it allows our server to react when a browser attempts to access the server by making a get request
-
-
+// __dirname is used so if run the server you will be directed to the correct path of the html page
 
 app.post("/", function(req, res) {
   // The app.post handles the POST request, which in this case made in the <form> in the html page
@@ -42,6 +41,29 @@ app.post("/", function(req, res) {
   // The num2 is from the name attribute in the .html page. The req.body.num1 is from the body-parser, which is parsing the info
   // Number is used to change the values to integer from string
   var addition = num_1 + num_2;
-  res.send("The rests from the server are " + addition);
+  res.send("The results from the server are " + addition);
+  // res.send is what will be shown after he POST request has been satisfied
+});
+
+
+app.get("/BMICalculator", function(req, res) {
+  res.sendFile(__dirname + "/BMICalculator.html");
+});
+// app.get is a method provided by express and it allows our server to react when a browser attempts to access the server by making a get request
+// __dirname is used so if run the server you will be directed to the correct path of the html page
+// When you go to http://localhost:3000/BMICalculator in the browser that will direct you exactly to the path of the /BMICalculator.html
+
+app.post("/BMICalculator.html", function(req, res) {
+  // The app.post handles the back end calculations. and that calculation will be Posted at /BMICalculator.html"
+  // console.log(req.body); -> is used to view all all the inputs from the customer or from the POST
+  // /BMICalculator.html is the location the POST of app.post is getting its infromation from
+  var weight = Number(req.body.weight);
+  // The num1 is from the name attribute in the .html page. The req.body.num1 is from the body-parser, which is parsing the info
+  // Number is used to change the values to integer from string
+  var height = Number(req.body.height);
+  // The num2 is from the name attribute in the .html page. The req.body.num1 is from the body-parser, which is parsing the info
+  // Number is used to change the values to integer from string
+  var bmi = (weight / (height**2));
+  res.send("The results from the server are " + bmi);
   // res.send is what will be shown after he POST request has been satisfied
 });
